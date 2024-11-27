d610d86ae78a5a2b22e2a9ba58da555ed482bdfb
vsgames42 <vsgames42@gmail.com>
2024-11-27 13:26:07 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log( "Connected to the DB.");
+})
+.catch((err)=>{
+    console.log(err);
+})
 
 const app = express();
 
 app.listen(3000,()=>{
-    console.log("Server started on port 3000!");
+    console.log("Server started on port 3000");
 })
+
+//vichu1722000smart
+// IC74zH8opz098YtK
