fa72e6987ccc687e9e685ec6c5adef0a33bc63a4
vsgames42 <vsgames42@gmail.com>
2024-11-30 11:31:42 +0530

    Add Redux Toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,15 @@
 import {useState} from 'react';
 import {Link, useNavigate} from "react-router-dom";
 import {Alert, Button, Label, Spinner, TextInput} from "flowbite-react";
+import {useDispatch} from "react-redux";
+import {signInStart, signInSuccess, signInFailure} from '../redux/user/userSlice'
+import { useSelector } from 'react-redux';
 
 function SignIn() {
 
     const [formData, setFormData] = useState({})
-    const [errorMessage, setErrorMessage] = useState(null)
-    const [loading, setLoading] = useState(false)
+    const {loading, error: errorMessage} = useSelector(state => state.user)
+    const dispatch = useDispatch()
     const navigate = useNavigate();
 
     const handleChange = (e) => {
@@ -16,11 +19,10 @@ function SignIn() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         if(!formData.email || !formData.password){
-            return setErrorMessage("Please fill out all fields")
+            return dispatch(signInFailure("Please fill out all fields"))
         }
         try{
-            setLoading(true)
-            setErrorMessage(null)
+            dispatch(signInStart())
             const res = await fetch('api/auth/signin', {
                 method: 'POST',
                 headers: {
@@ -30,17 +32,16 @@ function SignIn() {
             })
             const data = await res.json()
             if (data.success === false){
-                return setErrorMessage(data.message)
+                dispatch(signInFailure(data.message))
             }
-            setLoading(false)
             if(res.ok)
             {
+                dispatch(signInSuccess(data))
                 navigate('/')
             }
         }
         catch(err){
-            setErrorMessage(err.message)
-            setLoading(false)
+            dispatch(signInFailure(err.message))
         }
     }
 
